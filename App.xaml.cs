3b51b9488e48c082e433662485635a3e4cf3bc8e
Taisia Hognogi <daniela.hognogi@stud.ubbcluj.ro>
2024-11-26 19:50:21 +0200

    final laborator8

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,24 @@
-﻿namespace HognogiDanielaTaisiaLab7
+﻿using System;
+using HognogiDanielaTaisiaLab7.Data;
+using System.IO;
+
+namespace HognogiDanielaTaisiaLab7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
         public App()
         {
             InitializeComponent();
