0d0f860b4a99a62a23355405f8696ac1a451acd5
Taisia Hognogi <daniela.hognogi@stud.ubbcluj.ro>
2024-12-09 22:26:38 +0200

    Laborator 10 complet

--- Data/ShoppingListDatabase.cs
+++ Data/ShoppingListDatabase.cs
@@ -18,6 +18,7 @@ namespace HognogiDanielaTaisiaLab7.Data
             _database.CreateTableAsync<ShopList>().Wait();
             _database.CreateTableAsync<Product>().Wait();
             _database.CreateTableAsync<ListProduct>().Wait();
+            _database.CreateTableAsync<Shop>().Wait();
         }
         public Task<List<ShopList>> GetShopListsAsync()
         {
@@ -88,6 +89,26 @@ namespace HognogiDanielaTaisiaLab7.Data
          + " on P.ID = LP.ProductID where LP.ShopListID = ?",
          shoplistid);
 
+        }
+        public Task<List<Shop>> GetShopsAsync()
+        {
+            return _database.Table<Shop>().ToListAsync();
+        }
+        public Task<int> DeleteShopAsync(Shop shop)
+        {
+            return _database.DeleteAsync(shop);
+        }
+
+        public Task<int> SaveShopAsync(Shop shop)
+        {
+            if (shop.ID != 0)
+            {
+                return _database.UpdateAsync(shop);
+            }
+            else
+            {
+                return _database.InsertAsync(shop);
+            }
         }
     }
 }
---
+++ ShopPage.xaml.cs
@@ -0,0 +1,74 @@
+using HognogiDanielaTaisiaLab7.Models;
+using Plugin.LocalNotification;
+using HognogiDanielaTaisiaLab7.Data;
+
+namespace HognogiDanielaTaisiaLab7;
+
+public partial class ShopPage : ContentPage
+{
+    public ShopPage()
+    {
+        InitializeComponent();
+    }
+    async void OnSaveButtonClicked(object sender, EventArgs e)
+    {
+        var shop = (Shop)BindingContext;
+        await App.Database.SaveShopAsync(shop);
+        await Navigation.PopAsync();
+    }
+    async void OnDeleteButtonClicked(object sender, EventArgs e)
+    {
+        var shop = (Shop)BindingContext;
+
+        if (shop == null || shop.ID == 0)
+        {
+            await DisplayAlert("Error", "Shop not found or already deleted.", "OK");
+            return;
+        }
+
+        bool confirm = await DisplayAlert("Confirm Delete", $"Are you sure you want to delete the shop '{shop.ShopName}'?", "Yes", "No");
+
+        if (confirm)
+        {
+            // ?tergere din baza de date
+            await App.Database.DeleteShopAsync(shop);
+            await DisplayAlert("Deleted", "The shop has been deleted successfully.", "OK");
+
+            // Navigare înapoi la pagina anterioar?
+            await Navigation.PopAsync();
+        }
+    }
+
+    async void OnShowMapButtonClicked(object sender, EventArgs e)
+    {
+        var shop = (Shop)BindingContext;
+        var address = shop.Adress;
+        var locations = await Geocoding.GetLocationsAsync(address);
+
+        var options = new MapLaunchOptions{Name = "Magazinul meu preferat" };
+        var shoplocation = locations?.FirstOrDefault();
+        var myLocation = await Geolocation.GetLocationAsync();
+        /*   var myLocation = new Location(46.7731796289, 23.6213886738); 
+  //pentru Windows Machine */
+
+        var distance = myLocation.CalculateDistance(shoplocation, DistanceUnits.Kilometers);//location nu shoplocation
+
+        if (distance < 5)
+        {
+
+            var request = new NotificationRequest
+            {
+
+                Title = "Ai de facut cumparaturi in apropiere!",
+                Description = address,
+                Schedule = new NotificationRequestSchedule
+                {
+                    NotifyTime = DateTime.Now.AddSeconds(1)
+                }
+
+            };
+            LocalNotificationCenter.Current.Show(request);
+        }
+        await Map.OpenAsync(shoplocation, options);
+    }
+}
